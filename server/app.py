9450fbc797a8372dedd360eba2679cd35bda8231
GloriaAndonyaIT <gloriaandonyaa@gmail.com>
2025-06-20 22:15:45 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, request, make_response, jsonify
+from flask_cors import CORS
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
 
 from models import db, Plant
 
@@ -11,45 +11,75 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+CORS(app)
 migrate = Migrate(app, db)
+
 db.init_app(app)
 
-api = Api(app)
+@app.route('/')
+def index():
+    return '<h1>Plant Store API</h1>'
 
+# GET route to fetch all plants
+@app.route('/plants', methods=['GET'])
+def get_plants():
+    plants = Plant.query.all()
+    return jsonify([plant.to_dict() for plant in plants]), 200
 
-class Plants(Resource):
+# GET route to fetch a single plant
+@app.route('/plants/<int:id>', methods=['GET'])
+def get_plant(id):
+    plant = Plant.query.get_or_404(id)
+    return jsonify(plant.to_dict()), 200
 
-    def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+# POST route to create a new plant
+@app.route('/plants', methods=['POST'])
+def create_plant():
+    data = request.get_json()
+    
+    new_plant = Plant(
+        name=data.get('name'),
+        image=data.get('image'),
+        price=data.get('price'),
+        is_in_stock=data.get('is_in_stock', True)
+    )
+    
+    db.session.add(new_plant)
+    db.session.commit()
+    
+    return jsonify(new_plant.to_dict()), 201
 
-    def post(self):
-        data = request.get_json()
+# PATCH route to update an existing plant
+@app.route('/plants/<int:id>', methods=['PATCH'])
+def update_plant(id):
+    plant = Plant.query.get_or_404(id)
+    
+    data = request.get_json()
+    
+    # Update the plant's attributes based on what's provided
+    if 'name' in data:
+        plant.name = data['name']
+    if 'image' in data:
+        plant.image = data['image']
+    if 'price' in data:
+        plant.price = data['price']
+    if 'is_in_stock' in data:
+        plant.is_in_stock = data['is_in_stock']
+    
+    db.session.commit()
+    
+    return jsonify(plant.to_dict()), 200
 
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
-
-        db.session.add(new_plant)
-        db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
-
-
-api.add_resource(Plants, '/plants')
-
-
-class PlantByID(Resource):
-
-    def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
-
-
-api.add_resource(PlantByID, '/plants/<int:id>')
-
+# DELETE route to remove a plant
+@app.route('/plants/<int:id>', methods=['DELETE'])
+def delete_plant(id):
+    plant = Plant.query.get_or_404(id)
+    
+    db.session.delete(plant)
+    db.session.commit()
+    
+    # Return empty response with 204 status code
+    return '', 204
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
